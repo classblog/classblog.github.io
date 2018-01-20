0d2b3098967b1643db20c03c25c0d06187107e57
Igor <igor_zash@live.com>
2018-01-20 20:05:08 +0200

    Updated UI

--- js/master.js
+++ js/master.js
@@ -1,6 +1,9 @@
 let url = window.location.pathname;
 let resize = () => { 
 	$('#break').css('margin-top', $('nav').height());
+	let footer = $('#footer').height();
+	console.log(footer);
+	$('aside#content').css('min-height', `calc(100vh - ${footer})`);
 }
 $(window).resize(resize);
 resize();
