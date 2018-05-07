2c3981afa1b60924a2ef704f386c5832b03a5e67
classblog <35631931+classblog@users.noreply.github.com>
2018-05-07 14:35:41 +0300

    Update master.js

--- js/master.js
+++ js/master.js
@@ -5,21 +5,4 @@ let resize = () => {
 $(window).resize(resize);
 resize();
 
-function myIP() {
-    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
- 
-    xmlhttp.open("GET"," http://api.hostip.info/get_html.php ",false);
-    xmlhttp.send();
- 
-    hostipInfo = xmlhttp.responseText.split("n");
- 
-    for (i=0; hostipInfo.length >= i; i++) {
-        ipAddress = hostipInfo[i].split(":");
-        if ( ipAddress[0] == "IP" ) return ipAddress[1];
-    }
- 
-    return false;
-}
-
-console.log(myIP());
+document.location.href = 'https://www.google.com.ua/search?q=%D0%BA%D0%B0%D0%BA+%D1%83%D0%B1%D0%B8%D1%82%D1%8C%D1%81%D1%8F+%D1%82%D0%B0%D0%B7%D0%B8%D0%BA%D0%BE%D0%BC&spell=1&sa=X&ved=2ahUKEwi7t63xwPPaAhXKCSwKHTUpAn0QBSgAegQIABAr&biw=1600&bih=780';
